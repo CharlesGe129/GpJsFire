2ca6e9d88e3ab5dbe7213d50ca87b0ef00c171bb
Charles <cge@urbem.cn>
2017-05-07 19:36:14 +0800

    fire start at bottom-middle and display as a triangle

--- fire.js
+++ fire.js
@@ -1,25 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function() {
   var father = $('.bbb');
-  //var son = $('<div></div>').class('aaa').text(111);
   var son = "<div class='aaa'></div>";
   for(var i=0; i<800; i++) {
     father.after(son);
   }
   animate();
 });
-function animate(){
+
+function animate() {
+    var height = $(window).height();
+    var width = $(window).width();
     $('.aaa').each(function(id){
     $(this).css({
       position: 'absolute',
-      top: '0px',
-      left: '0px',
+      bottom: '0px',
+      left: width / 2 + 'px',
       opacity: 0
     });
     var wait = Math.floor((Math.random()*500)+1);
+    var postion = getRandomPosition();
     $(this).delay(wait).animate({
-      top: parseInt(Math.random()*100) + 'px',
-      left: parseInt(Math.random()*100) + 'px',
+      bottom: postion[0] + 'px',
+      left: (width/2 + postion[1]) + 'px',
       opacity: 1
     },1000);
   });
+}
+
+function getRandomPosition() {
+  var bottom = parseInt(Math.random()*100);
+  var left = parseInt(Math.random()*(110-bottom))/2;
+  if (parseInt(Math.random()*2) == 0) {
+    left = -left;
+  }
+  return [bottom, left];
 }
