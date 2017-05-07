19ead2722634067ca835bddbfd2dba83680127eb
Charles <cge@urbem.cn>
2017-05-07 19:50:06 +0800

    every point bounce back and force

--- fire.js
+++ fire.js
@@ -4,7 +4,10 @@ $(document).ready(function() {
   for(var i=0; i<800; i++) {
     father.after(son);
   }
-  animate();
+  for (var i=0; i<3; i++) {
+    animate();
+    //disappear();
+  }
 });
 
 function animate() {
@@ -17,11 +20,35 @@ function animate() {
       left: width / 2 + 'px',
       opacity: 0
     });
-    var wait = Math.floor((Math.random()*500)+1);
+    var wait = Math.floor((Math.random()*10000)+1);
     var postion = getRandomPosition();
     $(this).delay(wait).animate({
       bottom: postion[0] + 'px',
       left: (width/2 + postion[1]) + 'px',
+      opacity: 1
+    },1000);
+    $(this).animate({
+      bottom: '-20px',
+      opacity: 1
+    },1000);
+  });
+}
+
+function disappear() {
+    var height = $(window).height();
+    var width = $(window).width();
+    $('.aaa').each(function(id){
+    $(this).css({
+      position: 'absolute',
+      bottom: $(this).css('bottom'),
+      left: $(this).css('left'),
+      opacity: 0
+    });
+    var wait = Math.floor((Math.random()*500)+1);
+    var postion = getRandomPosition();
+    $(this).delay(wait).animate({
+      bottom: '0px',
+      left: width / 2 + 'px',
       opacity: 1
     },1000);
   });
