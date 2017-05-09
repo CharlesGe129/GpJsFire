67d5cb4fe64bdeb00507d80ccaa96fafe08eb7bc
Charles <cge@urbem.cn>
2017-05-09 13:16:17 +0800

    Use Three.js to generate 1,800 points formed in a triangle

--- fire.js
+++ fire.js
@@ -1,64 +1,143 @@
-$(document).ready(function() {
-  var father = $('.bbb');
-  var son = "<div class='aaa'></div>";
-  for(var i=0; i<800; i++) {
-    father.after(son);
-  }
-  for (var i=0; i<3; i++) {
-    animate();
-    //disappear();
-  }
-});
+// @see http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+window.requestAnimFrame = (function() {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function( /* function */ callback, /* DOMElement */ element) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+// set the scene size
+var WIDTH = 680,
+    HEIGHT = 500;
 
-function animate() {
-    var height = $(window).height();
-    var width = $(window).width();
-    $('.aaa').each(function(id){
-    $(this).css({
-      position: 'absolute',
-      bottom: '0px',
-      left: width / 2 + 'px',
-      opacity: 0
+// set some camera attributes
+var VIEW_ANGLE = 45,
+    ASPECT = WIDTH / HEIGHT,
+    NEAR = 0.1,
+    FAR = 10000;
+
+// get the DOM element to attach to
+// - assume we've got jQuery to hand
+var $container = $('#container');
+
+// create a WebGL renderer, camera
+// and a scene
+var renderer = new THREE.WebGLRenderer();
+var camera = new THREE.Camera(VIEW_ANGLE,
+    ASPECT,
+    NEAR,
+    FAR);
+var scene = new THREE.Scene();
+
+// the camera starts at 0,0,0 so pull it back
+camera.position.z = 300;
+
+// start the renderer - set the clear colour
+// to a full black
+renderer.setClearColor(new THREE.Color(0, 1));
+renderer.setSize(WIDTH, HEIGHT);
+
+// attach the render-supplied DOM element
+$container.append(renderer.domElement);
+
+// create the particle variables
+var particleCount = 1800,
+    particles = new THREE.Geometry(),
+    pMaterial = new THREE.ParticleBasicMaterial({
+        color: 0xFFFFFF,
+        size: 20,
+        /*
+        map: THREE.ImageUtils.loadTexture(
+            "partical.png"
+        ),
+        */
+        blending: THREE.AdditiveBlending,
+        transparent: true
     });
-    var wait = Math.floor((Math.random()*10000)+1);
-    var postion = getRandomPosition();
-    $(this).delay(wait).animate({
-      bottom: postion[0] + 'px',
-      left: (width/2 + postion[1]) + 'px',
-      opacity: 1
-    },1000);
-    $(this).animate({
-      bottom: '-20px',
-      opacity: 1
-    },1000);
-  });
+
+// now create the individual particles
+for (var p = 0; p < particleCount; p++) {
+
+    // create a particle with random
+    // position values, -250 -> 250
+    var pX = Math.random() * 500 - 250,
+        pY = Math.random() * 500 - 250,
+        pZ = Math.random() * 500 - 250,
+        particle = new THREE.Vertex(
+            new THREE.Vector3(pX, pY, pZ)
+        );
+    // create a velocity vector
+    particle.velocity = new THREE.Vector3(
+        0, // x
+        -Math.random(), // y
+        0); // z
+
+    // add it to the geometry
+    particles.vertices.push(particle);
 }
 
-function disappear() {
-    var height = $(window).height();
-    var width = $(window).width();
-    $('.aaa').each(function(id){
-    $(this).css({
-      position: 'absolute',
-      bottom: $(this).css('bottom'),
-      left: $(this).css('left'),
-      opacity: 0
-    });
-    var wait = Math.floor((Math.random()*500)+1);
-    var postion = getRandomPosition();
-    $(this).delay(wait).animate({
-      bottom: '0px',
-      left: width / 2 + 'px',
-      opacity: 1
-    },1000);
-  });
+// create the particle system
+var particleSystem = new THREE.ParticleSystem(
+    particles,
+    pMaterial);
+
+particleSystem.sortParticles = true;
+
+// add it to the scene
+scene.addChild(particleSystem);
+
+// animation loop
+function update() {
+    var allPosition = getAllPosition();
+
+    // add some rotation to the system
+    particleSystem.rotation.y += 0.01;
+
+    var pCount = particleCount;
+    while (pCount--) {
+        // get the particle
+        var particle = particles.vertices[pCount];
+
+        // check if we need to reset
+        if (particle.position.y < -200) {
+            particle.position.y = 200;
+            particle.velocity.y = 0;
+        }
+
+        // update the velocity
+        particle.velocity.y -= Math.random() * .1;
+
+        // and the position
+        particle.position.addSelf(
+            particle.velocity);
+        particle.position.x = allPosition[pCount][0];
+        particle.position.y = allPosition[pCount][1];
+        particle.position.z = allPosition[pCount][2];
+    }
+
+    // flag to the particle system that we've
+    // changed its vertices. This is the
+    // dirty little secret.
+    particleSystem.geometry.__dirtyVertices = true;
+
+    renderer.render(scene, camera);
+
+    // set up the next call
+    requestAnimFrame(update);
 }
 
-function getRandomPosition() {
-  var bottom = parseInt(Math.random()*100);
-  var left = parseInt(Math.random()*(110-bottom))/2;
-  if (parseInt(Math.random()*2) == 0) {
-    left = -left;
-  }
-  return [bottom, left];
+function getAllPosition() {
+    var all = [];
+    for (var i=0; i<1800; i++) {
+        var b = Math.random()*200;
+        var a = Math.random()*(200-b)+(100-(200-b)/2);
+        var c = 0;
+        all.push([a, b, c]);
+    }
+    return all;
 }
+
+requestAnimFrame(update);
